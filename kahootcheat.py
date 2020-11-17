83f5ac72de123f0c1e5fee5fea9c5892e4932cbc
DaniellTa <61436073+DaniellTa@users.noreply.github.com>
2020-11-16 23:07:57 -0500

    Update kahootcheat.py
    made shorter

--- kahootcheat.py
+++ kahootcheat.py
@@ -3,13 +3,9 @@ import json
 
 gameid = input("Enter game id: ")
 url = "https://play.kahoot.it/rest/kahoots/" + gameid
-
-s = ur.urlopen(url)
-sl = s.read()
-d = json.loads(sl)
-q = d["questions"]
-
+q = json.loads(ur.urlopen(url).read())["questions"]
 colours_list = ["red", "blue", "yellow", "green"]
+
 for index, slide in enumerate(q):
     if slide.get("type") == "quiz":
         for i in range(len(slide.get("choices"))):

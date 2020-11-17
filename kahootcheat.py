09ff86a72bb0ff478541945aab1106636cadb714
DaniellTa <61436073+DaniellTa@users.noreply.github.com>
2020-11-16 22:29:59 -0500

    Add files via upload

---
+++ kahootcheat.py
@@ -0,0 +1,18 @@
+import urllib.request as ur
+import json
+
+gameid = input("Enter game id: ")
+url = "https://play.kahoot.it/rest/kahoots/" + gameid
+
+s = ur.urlopen(url)
+sl = s.read()
+
+q = d["questions"]
+shape_list = ["red", "blue", "yellow", "green"]
+
+for index, slide in enumerate(q):
+    if slide.get("type") == "quiz":
+        for i in range(len(slide.get("choices"))):
+            if slide["choices"][i]["correct"] == True:
+                print("Question number: {}\n{}\n{}\n".format(
+                    index + 1, slide["choices"][i].get("answer"), shape_list[i]))

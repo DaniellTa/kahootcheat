08b08f45af4719e53001fb38035aa0db789fb490
DaniellTa <61436073+DaniellTa@users.noreply.github.com>
2021-12-03 23:09:45 -0500

    shortened two option edge case with one liner :)

--- kahootcheat.py
+++ kahootcheat.py
@@ -4,16 +4,12 @@ import json
 gameid = input("Enter game id: ")
 url = "https://play.kahoot.it/rest/kahoots/" + gameid
 q = json.loads(ur.urlopen(url).read())["questions"]
-four_list = ["red", "blue", "yellow", "green"]
-two_list = ["blue", "red"]
+colours = ["red", "blue", "yellow", "green"]
 
 for index, slide in enumerate(q):
     if slide.get("type") == "quiz":
         for i in range(len(slide.get("choices"))):
             if slide["choices"][i]["correct"]:
-                if(len(slide.get("choices")) == 2):
-                    colours_list = two_list
-                else:
-                    colours_list = four_list
+                colours_list = colours[:2][::-1] if len(slide.get("choices")) == 2 else colours
                 print("Question number: {}\n{}\n{}\n".format(
                     index + 1, slide["choices"][i].get("answer"), colours_list[i]))
